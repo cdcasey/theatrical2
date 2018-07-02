0720fff3b5351211d61be00acaddc984141f3e95
Chris Casey <cdcasey@gmail.com>
2018-07-02 15:37:39 -0500

    Dev deps and basic testing in place

---
+++ test/server.test.js
@@ -0,0 +1,16 @@
+process.env.PORT = 8888;
+process.env.NODE = 'test';
+const expect = require('chai').expect;
+const server = require('../server');
+const request = require('supertest')(server);
+
+describe('Existence of the server', () => {
+    it('should say hello', (done) => {
+        request.get('/')
+            .expect(200)
+            .end((err, res) => {
+                expect(res.text).to.include('hello')
+                done(err);
+            });
+    });
+});

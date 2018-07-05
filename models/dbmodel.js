5d7aff3138a86c066df87c0375dd6c03cdfe1862
Chris Casey <cdcasey@gmail.com>
2018-07-05 14:36:47 -0500

    Quick model refactor

---
+++ models/dbmodel.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const knex = require('../db/knex');
+const bcryptSync = require('bcrypt');
+
+class DBModel {
+
+    constructor() {
+        this.table = null;
+    }
+
+    all() {
+        return knex(this.table).orderBy('id');
+    }
+
+    getById(id) {
+        return knex(this.table).where('id', id).first();
+    }
+
+    create(data) {
+        data = this.validateData(data);
+        return knex(this.table).insert(data).returning('*');
+    }
+
+    update(id, data) {
+        data = this.validateData(data);
+        return this.getById(id).update(data);
+    }
+
+    delete(id) {
+        return this.getById(id).delete();
+    }
+
+    validateData(data) {
+        const validData = {};
+        for (const key in data) {
+            if (data[key]) {
+                validData[key] = data[key];
+            }
+            if (key === 'password') {
+                validData[key] = bcryptSync.hashSync(validData[key], 10);
+            }
+        }
+        return validData;
+    }
+}
+
+module.exports = DBModel;
--- models/users.js
+++ models/users.js
@@ -1,47 +1,12 @@
 'use strict';
 
-const knex = require('../db/knex');
-const bcryptSync = require('bcrypt');
+const DBModel = require('./dbmodel');
 
-class Users {
+class Users extends DBModel {
 
     constructor() {
+        super();
         this.table = 'users';
-    }
-
-    all() {
-        return knex(this.table).orderBy('id');
-    }
-
-    getById(id) {
-        return knex(this.table).where('id', id).first();
-    }
-
-    create(data) {
-        data = this.validateData(data);
-        return knex(this.table).insert(data).returning('*');
-    }
-
-    update(id, data) {
-        data = this.validateData(data);
-        return this.getById(id).update(data);
-    }
-
-    delete(id) {
-        return this.getById(id).delete();
-    }
-
-    validateData(data) {
-        const validData = {};
-        for (const key in data) {
-            if (data[key]) {
-                validData[key] = data[key];
-            }
-            if (key === 'password') {
-                validData[key] = bcryptSync.hashSync(validData[key], 10);
-            }
-        }
-        return validData;
     }
 }
 
